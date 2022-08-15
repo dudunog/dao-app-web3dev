1f6059263ed9dc21fcfe7acbedb7313bcd37c4c4
Eduardo Nogueira <eduardodudunog@gmail.com>
2022-08-14 23:36:21 -0300

    feat: home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  // Esse é o caso em que o usuário ainda não conectou sua carteira
+  // ao nosso site.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Bem-vind@s ao CartolaDAO - a DAO dos cartoleiros de plantão</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Conecte sua carteira
+        </button>
+      </div>
+    );
+  }
+
+  // Esse é o caso em que temos o endereço do usuário,
+  // o que significa que ele conectou sua carteira ao nosso site.
   return (
     <div className="landing">
-      <h1>Bem-vind@s à minha DAO</h1>
+      <h1>👀 carteira conectada, e agora?!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
